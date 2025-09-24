9127d5fea7ed7189d081e36cedcac7ea6415548b
Sam Packer <85488400+sampacker3@users.noreply.github.com>
2025-09-24 22:12:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -85,5 +85,5 @@ def health():
 
 
 if __name__ == "__main__":
-    # Gunicorn or Cloud Run will set host/port, this is for local testing
-    app.run(host="0.0.0.0", port=8080, debug=False)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
